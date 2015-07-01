4e0506ce013514bf849210dec5cce911cb90d392
mala23 <mail@mala23.me>
2015-07-01 11:19:46 +0200

    fuck static – meteor create

--- js/uglyness.js
+++
@@ -1,8 +0,0 @@
-däHalunk = Object.create(Halunka)
-däHalunk.config.width = innerWidth
-däHalunk.config.height = innerHeight
-
-addEventListener('load', function () {
-  document.body.appendChild(däHalunk.getCanvas())
-  däHalunk.init()
-})
