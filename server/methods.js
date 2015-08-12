6bb996604a0e12565f20245025d9581d35f8a61f
Kriegslustig <github@ls7.ch>
2015-08-12 17:55:35 +0200

    Implement the rate limiter

--- server/methods.js
+++ server/methods.js
@@ -1,7 +1,8 @@
 Meteor.methods({
   greetUs: function (sender, title, message) {
     check([sender, title, message], [String])
-    if(!isEMailAddress(sender)) throw new Meteor.Error('invalid-email-address', 'Invalid email address')
+    if(!isEMailAddress(sender)) throw new Meteor.Error('invalid-email-address', 'Good day dear Madam or Sir. You have passed an invalid email address to us.')
+    if(isOverRate(sender)) throw new Meteor.Error('over-rate', 'Greetings from our rate limiter: Wow! slow down, cowboy. Try again in 10 Minutes.')
     Emails.insert({
       from: sender,
       title: title,
@@ -16,3 +17,17 @@ Meteor.methods({
     })
   }
 })
+
+function isOverRate (sender, maxMailsInRate, maxRate) {
+  var now = (new Date).getTime()
+  maxRate = maxRate || 600000
+  maxMailsInRate = maxMailsInRate || 2
+  return (
+    Emails.find({
+      timestamp: {
+        $gt: now - maxRate
+      }
+    })
+      .count() >= maxMailsInRate
+  )
+}
