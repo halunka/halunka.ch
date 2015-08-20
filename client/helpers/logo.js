1b9f332b50416f1c6ebde1c1bae6a5eb071b7439
Kriegslustig <github@ls7.ch>
2015-08-20 14:06:21 +0200

    Make the logo animation work with mobile

--- client/helpers/logo.js
+++ client/helpers/logo.js
@@ -1,70 +1,104 @@
 Template.logo.onRendered(function () {
-  document.querySelector('.logo__wrapper').addEventListener(
+  var logoWrapper = document.querySelector('.logo__wrapper')
+  var gyroRotator = _.debounce(logoMover(
+    [
+      [document.querySelector('.logo'), 'rotateZ(2deg)', 1.2, -1.6, false],
+      [document.querySelector('.logo__innerTrs'), 'translateZ(14px)', 2.2, -2.6, false],
+    ]
+  ), 10)
+  var gyroCenter
+  addEventListener('deviceorientation', function (e) {
+    var curr = [e.gamma + 90, e.beta + 180]
+    if(!gyroCenter) gyroCenter = curr
+    gyroRotator(
+      [gyroCenter[0] - curr[0], gyroCenter[1] - curr[1]],
+      [180 - gyroCenter[0], 360 - gyroCenter[1]]
+    )
+  })
+  var mouseRotator = _.debounce(logoMover(
+    [
+      [document.querySelector('.logo'), 'rotateZ(2deg)', 1, 1, true],
+      [document.querySelector('.logo__innerTrs'), 'translateZ(14px)', 1.6, 1.6, true],
+    ]
+  ), 10)
+  logoWrapper.addEventListener(
     'mousemove',
-    _.debounce(logoMover(
-      [
-        [document.querySelector('.logo'), 'rotateZ(2deg)', 1],
-        [document.querySelector('.logo__innerTrs'), '', 1.6]
-      ]
-    ), 10)
+    function (e) {
+      mouseRotator(
+        [e.clientX - logoWrapper.offsetLeft, e.clientY - logoWrapper.offsetTop],
+        [logoWrapper.clientWidth, logoWrapper.clientHeight],
+        true
+      )
+    }
   )
 })
 
 function logoMover (elements) {
   elements.forEach(function (obj) {
-    obj[3] = rotator(obj[0], obj[1])
+    obj[5] = rotator(obj[0], obj[1], obj[4])
   })
-  return function (e) {
+  return function (pos, max) {
     elements.forEach(function (obj) {
-      obj[3](
-        rotation(getPos(e.clientX, obj[0].parentNode.clientWidth) * obj[2]),
-        rotation(getPos(e.clientY, obj[0].parentNode.clientHeight) * obj[2])
+      obj[5](
+        rotation(getPos(pos[0], max[0]) * obj[2]),
+        rotation(getPos(pos[1], max[1]) * obj[3] * -1)
       )
     })
   }
 }
 
 function rotation (pos) {
-  return Math.round(pos / 25 * 100 ) / 100
+  return pos / 25
 }
 
 function getPos (pos, len) {
   var rat = pos / len
-  if(rat > 1) rat = 1
-  if(rat < 0) rat = 0
   return rat * 100 - 50
 }
 
-function rotator (elem, def, dir) {
+function rotator (elem, def, animate) {
   var prop = ['rotate', prop].join('')
   var curr = [
     0,
     0
   ]
   return function (newX, newY) {
-    var step = [
-      (newX - curr[0]) / 3,
-      (newY - curr[1]) / 3
-    ]
-    _.zip(
-      _.range(curr[0], newX, step[0]),
-      _.range(curr[1], newY, step[1])
-    ).forEach(function (step, i) {
-      step[0] = step[0] || newX
-      step[1] = step[1] || newY
-      setTimeout(function () {
-        elem.style.transform =
-          [
-            def,
-            ['rotateY(', 'deg)']
-              .join(step[0]),
-            ['rotateX(', 'deg)']
-              .join(step[1])
-          ]
-            .join(' ')
-      }, 10 * i)
-    })
+    if(animate) {
+      var step = [
+        (newX - curr[0]) / 6,
+        (newY - curr[1]) / 6
+      ]
+      _.zip(
+        _.range(curr[0], newX, step[0]),
+        _.range(curr[1], newY, step[1])
+      ).forEach(function (step, i) {
+        step[0] = step[0] || newX
+        step[1] = step[1] || newY
+        setTimeout(function () {
+          rotate(elem, def, step[1], step[0])
+        }, 10 * i)
+      })
+    } else {
+      rotate(elem, def, newY, newX)
+    }
     curr[0] = newX
     curr[1] = newY
   }
+}
+
+function rotate (elem, def, x, y) {
+  var trs = [
+      def,
+      ['rotateY(', 'deg)']
+        .join(roundABit(y)),
+      ['rotateX(', 'deg)']
+        .join(roundABit(x))
+    ]
+      .join(' ')
+  elem.style.transform = trs
+  elem.style.webkitTransform = trs
+}
+
+function roundABit (n) {
+  return Math.round(n * 100) / 100
 }
