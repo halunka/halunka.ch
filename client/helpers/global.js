abb479a699f4367f234179cd41e700c1b36e37b2
Kriegslustig <github@ls7.ch>
2015-09-02 22:11:49 +0200

    Fix mobile user double tap issue

--- client/helpers/global.js
+++ client/helpers/global.js
@@ -1,1 +1,6 @@
 Template.registerHelper('not', function (expression) { return !expression })
+
+addEventListener('touchend', function (e) {
+  if (e.target.tagName === 'A') window.href = e.target.href
+  return false
+})
